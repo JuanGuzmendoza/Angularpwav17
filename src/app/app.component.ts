464f11742e8312b29db9b63cb2137b39e62b8572
JuanGuzmendoza <juanguzman10102005@gmail.com>
2024-06-17 11:55:13 -0500

    Cambio de versiones angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { OneSignal } from 'onesignal-ngx';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HttpClientModule, CommonModule],
+  templateUrl:'./app.component.html',
 })
-export class AppComponent {
-  title = 'Notificaciones';
+export class AppComponent implements OnInit {
+  data: { [key: string]: any } = {};
+  constructor(private http: HttpClient, private oneSignal: OneSignal) {
+    this.oneSignal.init({
+      appId: "7bb3c888-1958-46dc-b92b-1214692ed5b5",
+    });
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://api-medicina-abf75-default-rtdb.firebaseio.com/Data.json?_=' + new Date().getTime()).subscribe(response => {
+      this.data = response;
+      console.log("Conexion Exitosa")
+    })
+  }
 }
